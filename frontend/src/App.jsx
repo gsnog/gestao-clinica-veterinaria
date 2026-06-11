a4c1ea9b39cad10f90cb75bcbb29409056f0a096
Viviane Félix <viviane.felix@g.globo>
2026-06-11 07:24:00 -0300

    feat: configurar deploy automatico do frontend no GitHub Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 
